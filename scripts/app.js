3cbb9a8233001fd3cd0ee9261abd935a30671224
yuramishin <robothot@yandex.ru>
2025-05-14 00:24:16 +0300

    Добавлена корзина, реализованы эффекты + скрипты

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,37 @@
-const burgerBtn = document.querySelector('.header__burger'),
-header = document.querySelector('.header');
+const pageName = document.querySelector('meta[name="pagename"]').content;
+
+let burgerBtn,
+header,
+addBtnCartPosition,
+amountCartPosition,
+delBtnCartPosition,
+amountProducts,
+amountProduct,
+mainTotal,
+productTotal;
+
+const upProduct = () => {
+    amountProduct = Number(amountCartPosition.value);
+    permission = true //Тут будет запрос к серверу на наличие такого количества
+    if (permission && amountCartPosition.value < 99) {
+        amountCartPosition.value = amountProduct + 1
+    } else {
+        amountCartPosition.value = 99
+    }
+}
+
+const downProduct = () => {
+    amountProduct = Number(amountCartPosition.value);
+    if (amountProduct === 1) {
+        amountCartPosition.value = 1
+    } else {
+        amountCartPosition.value = amountProduct - 1
+    }
+}
+
+const delProduct = () => {
+    
+}
 
 const editStateMainMenu = () => {
     header.classList.toggle('header_close')
@@ -7,4 +39,30 @@ const editStateMainMenu = () => {
 }
 
 
-burgerBtn.addEventListener('click', editStateMainMenu)
+const cartInit = () => {
+    addBtnCartPosition = document.querySelector('.product__up');
+    amountCartPosition = document.querySelector('.product__amount');
+    delBtnCartPosition = document.querySelector('.product__down');
+
+    
+    productTotal = Number()
+
+
+    addBtnCartPosition.addEventListener('click', upProduct)
+    delBtnCartPosition.addEventListener('click', downProduct)
+}
+
+const adminInit = () => {
+    burgerBtn = document.querySelector('.header__burger');
+    header = document.querySelector('.header');
+
+    burgerBtn.addEventListener('click', editStateMainMenu)
+}
+
+
+startData = {
+    'cart': cartInit,
+    'admin': adminInit
+}
+
+startData[pageName]()
